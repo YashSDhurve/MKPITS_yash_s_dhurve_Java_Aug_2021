5b74850087bb793f2bd4c1910c310dd8361846dc
YashSDhurve <yash43075@gmail.com>
2021-12-15 20:35:05 +0530

    Additonjdbc file added

---
+++ java_programs/Additionjdbc.java
@@ -0,0 +1,7 @@
+public class Additoonjdbc {
+    public static void main(String[] arg){
+        int num1=3,num2=4,sum;
+        sum=num1+num2;
+        System.out.println("Additon of two numbers = "+sum);
+    }
+}
